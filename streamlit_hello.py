7ab3c31747eccca65a223c9dcc2ef41ea4ee272d
susymech1 <88503649+susymech1@users.noreply.github.com>
2022-05-23 08:26:12 -0400

    New updates

--- streamlit_hello.py
+++ streamlit_hello.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
+import plotly.express as px
 
 
 
@@ -10,8 +11,11 @@ from matplotlib.ticker import StrMethodFormatter
 st.set_page_config(layout="wide")
 
 # dashboard title
-st.title(" Clinical risk alert using Machine Learning")
+st.title(" Clinical risk alert using Machine Learning(ML)")
+st.markdown("<h6 style='text-align: left; color: black;'>ML model which predicts risk-score against each visit for certain disease condition. It then finally calculate weighted risk-score for patients:  </h6>", unsafe_allow_html=True)
 #st.subheader("Machine Learning model for predicting risk-score of certain disease condition ")
+
+
 st.markdown("<hr/>", unsafe_allow_html=True)
    
 st.sidebar.subheader("Select disease & patient")
@@ -50,51 +54,63 @@ st.sidebar.subheader("Select disease & patient")
 
 option = st.sidebar.selectbox(
     "disease condition",
-    ('Acute bronchitis','Asthma', 'Anemia','Viral sinusitis','Chronic congestive heart failure'))  
+    ('Acute bronchitis','Chronic congestive heart failure','Asthma', 'Anemia'))  
 
 
 if option == 'Acute bronchitis':
      df = pd.read_csv("acute_bronchitis_prediction.csv")
+     patient_filter = st.sidebar.selectbox(
+            "patient id", 
+            ('019319e2-e1e6-4691-a74f-11285ff10b81','015a6eb2-ba4a-46e7-a093-44aa1c923a95'))
      
-elif option == 'Asthma': 
-     df = pd.read_csv("asthma_prediction.csv")
-         
-elif option == 'Anemia':     
-    df = pd.read_csv("anemia_prediction.csv")
+     if patient_filter == '019319e2-e1e6-4691-a74f-11285ff10b81':
+         start = 7572
+ 
     
-elif option == 'Viral sinusitis': 
-     df = pd.read_csv("viral_sinusitis_prediction.csv")
+     elif patient_filter == '015a6eb2-ba4a-46e7-a093-44aa1c923a95':
+         start = 7556
          
 elif option == 'Chronic congestive heart failure':     
     df = pd.read_csv("chronic_congestive_heart_failure_prediction.csv")
+    patient_filter = st.sidebar.selectbox(
+            "patient id", 
+            ('970692ab-1eb1-4eb1-a528-88dbaad13aa6','0372c553-9f75-4167-ac3f-bbf89e6bed4c'))
+    if patient_filter == '970692ab-1eb1-4eb1-a528-88dbaad13aa6':
+        start = 2350
+ 
     
-patient_filter = st.sidebar.selectbox("patient id", pd.unique(df["PATIENT"]))
+    elif patient_filter == '0372c553-9f75-4167-ac3f-bbf89e6bed4c':
+        start = 3640
+    
+    
+     
+elif option == 'Asthma': 
+     df = pd.read_csv("asthma_prediction.csv")
+     patient_filter = st.sidebar.selectbox(
+            "patient id", 
+            ('970692ab-1eb1-4eb1-a528-88dbaad13aa6','0372c553-9f75-4167-ac3f-bbf89e6bed4c'))
+         
+elif option == 'Anemia':     
+    df = pd.read_csv("anemia_prediction.csv")
+    patient_filter = st.sidebar.selectbox(
+            "patient id", 
+            ('970692ab-1eb1-4eb1-a528-88dbaad13aa6','0372c553-9f75-4167-ac3f-bbf89e6bed4c'))
+            
+
+    
+    
 
 
-patient_filter = st.sidebar.selectbox(
-    "patient_id_common",
-    ('05ce1ed1-df78-451c-aa13-5912ad75cd8a',
- '09911b10-bafe-4d92-8762-b5778bd53aba',
- '09a55dbb-d1db-48a2-a6e1-8d9b168a9d1d',
- '0ac0182b-f424-4a2c-a1cb-65fb4036fbb9',
- '0082e62a-b695-463d-bca9-4b4da77a21af',
- '009e7163-9422-4706-a38f-dd6c323108ca',
- '0143acdb-2aaa-41e1-8f4a-e899b77e0e60',
- '080cd686-24fa-4864-a60b-e11cc52111e8',
- '073618d0-e5b7-451f-8aaa-9217000901df',
- '06048af1-6d9f-4e30-91dc-9a039c1a2fd0',
- '05af9427-0744-4fb3-9a99-929b6ec96bb7',
- '09d139f0-08a4-446b-967d-dd7dc482ebd6',
- '03c20045-2d5f-4c0c-a0e3-eb8682f4350b',
- '0a936f35-7ffc-452c-8814-ac511d8b3087'))  
-    
-    
+# patient_filter = st.sidebar.selectbox("patient id", pd.unique(df["PATIENT"]))
+
+
+
 df['Prediction'] = round(df['Prediction'], 2)
-df1 = df[(df["Prediction"]>= 0.9)]
-df2 = df[(df["Prediction"]>= 0.7) & (df["Prediction"]<0.9)]
+#df1 = df[(df["Prediction"]>= 0.9)]
+#df2 = df[(df["Prediction"]>= 0.7) & (df["Prediction"]<0.9)]
 df_each = df.loc[df["PATIENT"] == patient_filter].sort_values('START_YEAR',ascending = True)
-df1_each = df1.loc[df1["PATIENT"] == patient_filter]
-df2_each = df2.loc[df2["PATIENT"] == patient_filter]
+#df1_each = df1.loc[df1["PATIENT"] == patient_filter]
+#df2_each = df2.loc[df2["PATIENT"] == patient_filter]
 
 
 #st.markdown("<h6 style='text-align: left; color: black;'>Cumulative risk score based on last 3 visits:  </h6>", unsafe_allow_html=True)
@@ -130,9 +146,6 @@ with fig_col1:
         st.error("High-risk patient - Immediate intervention is needed")
         
         
-st.markdown("<hr/>", unsafe_allow_html=True)
-
-
 with st.expander("Patient demographic details"):
      st.markdown('**Gender**: ' + str(df_each.iloc[-1]['GENDER']))
      st.markdown('**Race**: ' + str(df_each.iloc[-1]['RACE']))
@@ -142,88 +155,157 @@ with st.expander("Patient demographic details"):
      st.markdown('**Age**@Last record: ' + str(round(df_each.iloc[-1]['patient_age']))+' '+'yr')
      st.markdown('**Zip**@Last record: ' + str(df_each.iloc[-1]['ZIP']))
 
- 
-kpi1, kpi2, kpi3 = st.columns(3)
-
-kpi1.metric(
-   label="Number of visits",
-    value=df_each.shape[0])
-
-kpi2.metric(
-   label="High-risk visits",
-   value=df1_each.shape[0])
-
-kpi3.metric(
-    label="Moderate-risk visits",
-    value=df2_each.shape[0])
 
 st.markdown("<hr/>", unsafe_allow_html=True)
 
+st.markdown("<h6 style='text-align: left; color: black;'>Sample test visit:  </h6>", unsafe_allow_html=True)
 
-     
-#create two columns for charts
-fig_col1, fig_col2 = st.columns(2)
+st.text('current visit')
+ 
+#kpi1, kpi2, kpi3 = st.columns(3)
 
+#kpi1.metric(
+#   label="Number of visits",
+#    value=df_each.shape[0])
+
+#kpi2.metric(
+#   label="High-risk visits",
+#   value=df1_each.shape[0])
+
+#kpi3.metric(
+#    label="Moderate-risk visits",
+#    value=df2_each.shape[0])
+
+#st.markdown("<hr/>", unsafe_allow_html=True)
+ 
+ 
+
+#kpi2, kpi6 = st.columns(2)   
+
+#kpi1.metric(
+#   label="Visit year",
+#    value=df.iloc[2350:2351,1].values[0]) 
+
+#kpi2.metric(
+#   label="Patient Age",
+#    value=df.iloc[2350:2351,6].values[0]) 
+
+#kpi3.metric(
+#   label="Encounter type",
+#    value=df.iloc[2350:2351,0].values[0]) 
+
+#kpi4.metric(
+#   label="Past diagnosis",
+#   value=df.iloc[2350:2351,9].values[0]) 
+
+#kpi5.metric(
+#   label="Present diagnosis",
+#    value=df.iloc[2350:2351,10].values[0]) 
+
+#with kpi2:
+  
+#kpi6.metric(
+#   label="Predicted risk-score",
+#    value=df.iloc[2350:2351,8].values[0]) 
+
+
+
+
+
+df1 = df.drop(['PATIENT','RACE','GENDER','ZIP','label','Prediction'], 1)
+#row = pd.IndexSlice[df1.index[df1.index == 2350], 3:5])
+#df1.style.applymap(df_style, subset=df1.iloc[2350:2351,:])
+df1.rename(columns = {'ENCOUNTERCLASS':'ENCOUNTER', 'START_YEAR':'YEAR','patient_age':'AGE','past_conditions':'PAST DIAGNOSIS','present_conditions':'CURRENT DIAGNOSIS'}, inplace = True)
+st.dataframe(df1.iloc[start:start+1,:])   
+
+
+if st.button('Predicted risk-score for the next visit'):
+    result = df.iloc[start:start+1,8].values[0]
+    st.write(result)
+    st.text('real next visit')
+    st.dataframe(df1.iloc[start+1:start+2,:].drop(['PAST DIAGNOSIS'], 1))
+    
+#if st.button('What is the real next visit ?'):
+#   st.dataframe(df1.iloc[2351:2352,:])
+    
+st.markdown("<hr/>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: left; color: black;'>Risk-score & Encounter type:</h6>", unsafe_allow_html=True)     
+
+
+fig_col1, fig_col2 = st.columns(2)  
+#with st.expander("Predicted risk-score vs visit year"):
 with fig_col1:
-    st.markdown("<h4 style='text-align: center; color: black;'>Risk score for different visits </h4>", unsafe_allow_html=True)
-    fig = plt.figure(figsize=(3,2))
+    #st.markdown("<h4 style='text-align: center; color: black;'>Risk score for different visits </h4>", unsafe_allow_html=True)
+    st.text("Predicted risk-score vs visit year")
+    fig = plt.figure(figsize=(3, 2))
     ax = fig.add_subplot(1,1,1)
-    ax.scatter(df_each['START_YEAR'],df_each["Prediction"],s=5)
+    ax.scatter(df_each['START_YEAR'],df_each["Prediction"],c='black',s=10)
+    
     ax.set_xlabel("Visit year",fontsize= 6)
     ax.set_ylabel("Risk score",fontsize= 6)
-    ax.xaxis.set_tick_params(labelsize=4)
-    ax.yaxis.set_tick_params(labelsize=4)
-    #ax.set_xlim([df_each.iloc[0]['START_YEAR'],df_each.iloc[-1]['START_YEAR']])
-    ax.set_ylim([0,1])
+    ax.xaxis.set_tick_params(labelsize=6)
+    ax.yaxis.set_tick_params(labelsize=6)
+    ax.set_xlim([df_each.iloc[0]['START_YEAR'],df_each.iloc[-1]['START_YEAR']])
+    ax.set_ylim([-0.1,1.1])
     ax.xaxis.set_major_formatter(StrMethodFormatter('{x:.0f}')) # No decimal places
+    st.pyplot(fig)
+    #fig = px.scatter(df_each['START_YEAR'],df_each["Prediction"])
     
     
-    x =  [df_each['START_YEAR'].values[0],df_each['START_YEAR'].values[df_each.shape[0]-1]]
-    y1 = [0.9]
-    y2 = [1.0]
-    y3 = [0.7]
-    y4 = [0.9]
+   # x =  [df_each['START_YEAR'].values[0],df_each['START_YEAR'].values[df_each.shape[0]-1]]
+   # y1 = [0.9]
+   # y2 = [1.0]
+   # y3 = [0.7]
+   # y4 = [0.9]
    
 
-    ax.fill_between(x, y1, y2, facecolor="red", color='red', alpha=0.15)          
-    ax.fill_between(x, y3, y4,facecolor="orange",color='orange',alpha=0.15) 
+    #ax.fill_between(x, y1, y2, facecolor="red", color='red', alpha=0.15)          
+    #ax.fill_between(x, y3, y4,facecolor="orange",color='orange',alpha=0.15) 
     
     
-    ax.text(df_each['START_YEAR'].values[df_each.shape[0]-2], 0.95, 'High-risk(>90%)',fontsize= 6)
-    ax.text(df_each['START_YEAR'].values[df_each.shape[0]-2], 0.8, 'Moderate-risk(70-90%)',fontsize= 6)
+    #ax.text(df_each['START_YEAR'].values[df_each.shape[0]-2], 0.95, 'High-risk(>90%)',fontsize= 6)
+    #ax.text(df_each['START_YEAR'].values[df_each.shape[0]-2], 0.8, 'Moderate-risk(70-90%)',fontsize= 6)
     
+    #st.plotly_chart(fig, use_container_width=True)
     
-    st.pyplot(fig)
-    
+
    
+   
+#with st.expander("Encounter class distribution"):
 with fig_col2:
-    st.markdown("<h4 style='text-align: center; color: black;'>Encounter class for all the visits </h4>", unsafe_allow_html=True)
-    fig = plt.figure(figsize=(3,2))
+    #st.markdown("<h4 style='text-align: center; color: black;'>Encounter class for all the visits </h4>", unsafe_allow_html=True)
+    st.text("Encounter class distribution")
+    fig = plt.figure(figsize=(3, 2))
     ax = fig.add_subplot(1,1,1)
     
     y = df_each.groupby('ENCOUNTERCLASS')['PATIENT'].count().values
     mylabels = df_each.groupby('ENCOUNTERCLASS')['PATIENT'].count().index.values
     ax.pie(y, labels = mylabels,autopct = "%0.2f%%", textprops={'fontsize': 3}) 
     st.pyplot(fig)
-    
 
 
-st.markdown("<h6 style='text-align: left; color: black;'>Get the patient record of high-risk & moderate-risk visits </h6>", unsafe_allow_html=True)
+
+#st.markdown("<h6 style='text-align: left; color: black;'>Get the patient record of high-risk & moderate-risk visits </h6>", unsafe_allow_html=True)
 
 
-with st.expander("High-risk visits"):
-    df1_each_short = df1_each.drop(['PATIENT'], 1)
-    st.dataframe(df1_each_short)
+#with st.expander("High-risk visits"):
+#    df1_each_short = df1_each.drop(['PATIENT'], 1)
+#    st.dataframe(df1_each_short)
      
-with st.expander("Moderate-risk visits"):
-    df2_each_short = df2_each.drop(['PATIENT'], 1)
-    st.dataframe(df2_each_short)
+#with st.expander("Moderate-risk visits"):
+#    df2_each_short = df2_each.drop(['PATIENT'], 1)
+#    st.dataframe(df2_each_short)
      
 
+st.markdown("<hr/>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: left; color: black;'>Dataset & modeling approach:</h6>", unsafe_allow_html=True)     
 
+with st.expander("Dataset description"):
+    st.text('The dataset is generated by Synthea, which is a Synthetic Patient Population Simulator')
+    st.text('The patient’s encounter records and demographic information are used from electronic health record (EHR)')
 
-
-
+with st.expander("ML modeling approach"):
+    st.text(1)
 
 
 
